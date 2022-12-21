920f0350d20ef235daafdd91c5dad9f3a504658a
Manuel <manu13dkv@gmail.com>
2022-12-21 19:52:33 +0100

    Apartado 2

--- cola_observador.cc
+++ cola_observador.cc
@@ -15,12 +15,34 @@ ColaObservador::ColaObservador (Ptr<OnOffApplication> app_fuente, Ptr<Queue<Pack
 
   m_cola_dispo = cola_dispo;
   m_cola_tcl = cola_tcl;
+  m_cuenta = 0;
 }
 
 
 void
 ColaObservador::PaqueteTx_fuente (Ptr<const Packet> paquete)
 {
+  m_cuenta++;
+
+  m_media_tx.Update((Simulator::Now() - m_prev_time).GetSeconds());
+  m_prev_time = Simulator::Now();
+
+  NS_LOG_DEBUG (paquete << "Paquete Tx:\n"<<
+                "=================================================================");
   NS_LOG_DEBUG (paquete << "Número de paquetes en la cola del dispositivo: " << m_cola_dispo->GetNPackets());
   NS_LOG_DEBUG (paquete << "Número de paquetes en la cola tcl: " << m_cola_tcl->GetNPackets());
+}
+
+
+double
+ColaObservador::GetMediaIntervaloTx (void)
+{
+  return m_media_tx.Avg();
+}
+
+
+double
+ColaObservador::GetNPaquetesTx (void)
+{
+  return m_cuenta;
 }
--- practica06.cc
+++ practica06.cc
@@ -40,24 +40,34 @@
 #include "retardo.h"
 #include "cola_observador.h"
 
+#include "ns3/random-variable-stream.h"
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("Practica06");
 
-void escenario(uint32_t num_fuentes, double n_max_paq, Time duracion_simulacion, Time int_envio_cliente, double tam_paq, DataRateValue c_transmision, double tam_cola);
+void escenario(uint32_t num_fuentes, /*double n_max_paq,*/ Time duracion_simulacion, /*Time int_envio_cliente,*/
+                    Time duracion_comunicacion,
+                     DataRate tasa_envio, Ptr<ExponentialRandomVariable> t_on, Ptr<ExponentialRandomVariable> t_off,
+                     uint64_t tam_paq, DataRateValue c_transmision, double tam_cola);
 
 int main(int argc, char *argv[])
 {
     Time::SetResolution(Time::NS);
   
     uint32_t num_fuentes= 2;  // Último dígito del número de grupo. 
-    std::string int_envio_cliente_value = "1ms";  //Por defecto es el intervalo mínimo, cercano al teórico al que no se pierden paquetes.
-    double tam_paq_value = 8192;
+    //std::string int_envio_cliente_value = "1ms";  //Por defecto es el intervalo mínimo, cercano al teórico al que no se pierden paquetes.
+    uint64_t tam_paq_value = 8192;
+    std::string tasa_envio_value = "32kbps";
+    std::string t_on_value = "350ms";
+    std::string t_off_value = "650ms";
+    std::string duracion_comunicacion_value = "50s";
+
     double tam_cola_inicial = 50;
-    int num_curvas = 4;
-    int num_puntos = 5; // Numero de veces que se decrementará la cola del conmutador.
-    double error = 2;
-    double n_max_paq = ( 400 + 100*( 13 % 4 ) ); //13 -> número de grupo
+    //int num_curvas = 4;
+    //int num_puntos = 5; // Numero de veces que se decrementará la cola del conmutador.
+    //double error = 2;
+    //double n_max_paq = ( 400 + 100*( 13 % 4 ) ); //13 -> número de grupo
 
     //Capacidad de los enlaces
     std::string c_transmision_value = "1Mbps";
@@ -66,23 +76,38 @@ int main(int argc, char *argv[])
     // Configuracion linea de comandos
     CommandLine cmd(__FILE__);
 
-    cmd.AddValue("num_curvas", "Veces que aumentará el número de fuentes", num_curvas);
-    cmd.AddValue("num_puntos", "Veces que se decrementará la cola de paquetes", num_puntos);
+    //cmd.AddValue("num_curvas", "Veces que aumentará el número de fuentes", num_curvas);
+    //cmd.AddValue("num_puntos", "Veces que se decrementará la cola de paquetes", num_puntos);
+    //cmd.AddValue("int_envio_cliente", "Intervalo de envio de paquetes del cliente", int_envio_cliente_value);
+    //cmd.AddValue("error", "Error en las gráficas", error);
+    //cmd.AddValue("n_max_paq", "Número máximo de paquetes a enviar", n_max_paq);
+    
     cmd.AddValue("num_fuentes", "Numeros de fuentes del escenario a crear", num_fuentes);
-    cmd.AddValue("int_envio_cliente", "Intervalo de envio de paquetes del cliente", int_envio_cliente_value);
     cmd.AddValue("tam_paq", "Tamano del paquete UDP", tam_paq_value);
     cmd.AddValue("tam_cola", "Tamano de cola en número de paquetes", tam_cola_inicial);
-    cmd.AddValue("error", "Error en las gráficas", error);
     cmd.AddValue("c_transmision", "Capacidad de transmision de los enlaces del conmutador", c_transmision_value);
-    cmd.AddValue("n_max_paq", "Número máximo de paquetes a enviar", n_max_paq);
-    
+    cmd.AddValue("t_on", "Duración media estado ON", t_on_value);
+    cmd.AddValue("t_off", "Duración media estado OFF", t_off_value);
+    cmd.AddValue("tasa_envio", "Tasa de envio de las fuentes", tasa_envio_value);
+    cmd.AddValue("duracion_comunicacion", "Duración de la comunicación", duracion_comunicacion_value);
+
     cmd.Parse(argc, argv);
 
     DataRateValue c_transmision = DataRateValue(c_transmision_value);
-    Time int_envio_cliente = Time(int_envio_cliente_value);
+    //Time int_envio_cliente = Time(int_envio_cliente_value);
+
+    DataRate tasa_envio = DataRate(tasa_envio_value);
+
+    Ptr<ExponentialRandomVariable> t_on  = CreateObject<ExponentialRandomVariable> ();
+    t_on->SetAttribute ("Mean", DoubleValue (Time(t_on_value).GetSeconds ()));
+
+    Ptr<ExponentialRandomVariable> t_off = CreateObject<ExponentialRandomVariable> ();
+    t_off->SetAttribute ("Mean", DoubleValue (Time(t_off_value).GetSeconds ()));
+
+    Time duracion_comunicacion = Time(duracion_comunicacion_value);
 
     //Duración de la simulación acorde al número máximo de paquetes a enviar.
-    Time duracion_simulacion = Time( n_max_paq * int_envio_cliente + Time("10ms") ); //Tiempo prudencial para establecer conexión
+    Time duracion_simulacion = Time( duracion_comunicacion + Time("100ms") ); //Tiempo prudencial para establecer conexión
 
     //Gráfica -> Pérdida de paquetes entre tamano de cola
     /*std::string fileNameWithNoExtension_perdidas = "practica05_perdidas";
@@ -143,16 +168,22 @@ int main(int argc, char *argv[])
     fichero_retardo.close();
     */
 
-   escenario(num_fuentes, n_max_paq, duracion_simulacion, int_envio_cliente, tam_paq_value, c_transmision, tam_cola_inicial);
+   escenario(num_fuentes, duracion_simulacion, duracion_comunicacion,
+                    tasa_envio, t_on, t_off,
+                     tam_paq_value, c_transmision, tam_cola_inicial);
 
 }
 
-void escenario(uint32_t num_fuentes, double n_max_paq, Time duracion_simulacion, Time int_envio_cliente, double tam_paq, DataRateValue c_transmision, double tam_cola)
+void escenario(uint32_t num_fuentes, /*double n_max_paq,*/ Time duracion_simulacion, 
+                Time duracion_comunicacion,
+                /*Time int_envio_cliente,*/ DataRate tasa_envio,
+                Ptr<ExponentialRandomVariable> t_on, Ptr<ExponentialRandomVariable> t_off,
+                uint64_t tam_paq, DataRateValue c_transmision, double tam_cola)
 {
 
+    //--> Nodos
+    //=====================================================================================================
     NodeContainer clientes_c(num_fuentes);
-
-    //Creacion de nodo sumidero
     Ptr<Node> sumidero=CreateObject<Node>();
 
     NodeContainer c_todos;
@@ -168,7 +199,6 @@ void escenario(uint32_t num_fuentes, double n_max_paq, Time duracion_simulacion,
     PuenteHelper puent;
     Ptr<Node> puente=CreateObject<Node>();
     puente=puent.Puentehelper(c_todos,c_dispositivos, c_transmision);
-    
 
     //Configuracion de la capa de aplicacion del sumidero
 
@@ -180,38 +210,33 @@ void escenario(uint32_t num_fuentes, double n_max_paq, Time duracion_simulacion,
     Ipv4InterfaceContainer c_interfaces =h_direcciones.Assign (c_dispositivos);
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
     
-    //Capa de aplicación para las fuentes 
+    InetSocketAddress direcciones(c_interfaces.GetAddress(0,0),puerto);
     
-    DataRate data_rate = DataRate(( tam_paq * 8 ) / int_envio_cliente.GetSeconds());
-    NS_LOG_INFO("Régimen Binario de las fuentes : " << data_rate);
+    //--> OnOfApplication
+    //=====================================================================================================
+    OnOffHelper onoff("ns3::UdpSocketFactory",direcciones);
+    
+    onoff.SetAttribute("OnTime", PointerValue(t_on));
+    onoff.SetAttribute("OffTime", PointerValue(t_off));
+    onoff.SetAttribute("DataRate", DataRateValue(tasa_envio));
+    onoff.SetAttribute("PacketSize", UintegerValue(tam_paq));
+    onoff.SetAttribute("StopTime", TimeValue(duracion_comunicacion));
 
-    InetSocketAddress dir(c_interfaces.GetAddress(0,0),puerto);
-    OnOffHelper onoff("ns3::UdpSocketFactory",dir);
-    onoff.SetConstantRate(data_rate,tam_paq);
     ApplicationContainer app_c=onoff.Install(clientes_c);
-
-    for (uint64_t i = 0; i < num_fuentes; i++){
-        app_c.Get(i)->GetObject<OnOffApplication>()->SetMaxBytes(n_max_paq*tam_paq);
-        app_c.Get(i)->SetStopTime(duracion_simulacion);
-    }    
-
-   
-
-    //Obtenemos la cola del dispositivo de red de una de las fuentes.
+    
+    //-->Control de tráfico.
+    //=====================================================================================================
     Ptr<Queue<Packet>> cola_disp_fuente = c_dispositivos.Get(1)->GetObject<CsmaNetDevice>()->GetQueue();
 
-    //control de tráfico.
     Ptr<TrafficControlLayer> tcl = clientes_c.Get(0)->GetObject<TrafficControlLayer> ();
     Ptr<QueueDisc> cola_tcl_fuente = tcl->GetRootQueueDiscOnDevice(c_dispositivos.Get(1));
+    cola_tcl_fuente->SetMaxSize(QueueSize("10p"));
  
-    //Creamos el observador de las colas tcl y transmisión.
-    ColaObservador* cola_observador = new ColaObservador(app_c.Get(0)->GetObject<OnOffApplication>(), cola_disp_fuente, cola_tcl_fuente);
+    ColaObservador* observador_onoff = new ColaObservador(app_c.Get(0)->GetObject<OnOffApplication>(), cola_disp_fuente, cola_tcl_fuente);
 
     //Valor inicial de la cola
     //cola_fuente->SetMaxSize(QueueSize(ns3::PACKETS, tam_cola));
-
     //Retardo retardo(app_c, num_fuentes, app_container_sumidero.Get(0)->GetObject<UdpServer>());
-
     //cola_servidor->ResetStatistics();
     
     NS_LOG_INFO("Arranca Simulación");
@@ -219,17 +244,20 @@ void escenario(uint32_t num_fuentes, double n_max_paq, Time duracion_simulacion,
     Simulator::Run();
 
     NS_LOG_INFO("Número de fuentes: "<< num_fuentes);
+    NS_LOG_INFO("Régimen Binario de las fuentes : " << tasa_envio);
     NS_LOG_INFO("Tamaño de cola del dispositivo: "<< cola_disp_fuente->GetMaxSize().GetValue());
     NS_LOG_INFO("Tamaño de cola de control de tráfico: "<< cola_tcl_fuente->GetMaxSize().GetValue());
+    NS_LOG_INFO("Duracion de la comunicación VoIP: "<< duracion_comunicacion);
+    NS_LOG_INFO("OnOffApplication-> Comprobación media T.Total envío de un paquete: "<< observador_onoff->GetMediaIntervaloTx()<<" (s)");
+    NS_LOG_INFO("Número de paquetes transmitidos por una fuente: "<< observador_onoff->GetNPaquetesTx());
+    
+
     //NS_LOG_INFO("Número de paquetes recibidos por el servidor UDP: " << observador_udp->TotalPaquetes());
     //NS_LOG_INFO("Número de paquetes recibidos por la cola del conmutador: " << cola_servidor->GetTotalReceivedPackets());
     //NS_LOG_INFO("Numero de paquetes descartados por la cola: "<< cola_fuente->GetTotalDroppedPackets());
     //NS_LOG_INFO("Número total de paquetes transmitidos: "<< retardo.TotalPaquetesTx());
-
     //double porcentaje_paquetes_perdidos = 100*(double)cola_servidor->GetTotalDroppedPackets() / (double)(retardo.TotalPaquetesTx());
-    
     //NS_LOG_INFO("Porcentaje de paquetes perdidos: "<< porcentaje_paquetes_perdidos << "%");
-
     //NS_LOG_INFO("Retardo medio: "<< retardo.RetardoMedio());
     NS_LOG_INFO("Fin simulación");
 
